8559062440a65e59344dbe077ef152d5b26d47d3
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2019-08-29 06:12:02 +0700

    Change to upgrade ncurses and build env

--- distro/nixos/build.nix
+++ distro/nixos/build.nix
@@ -1,20 +1,24 @@
-args@{ stdenv, lib, fetchurl, fetchFromGitHub, rustPlatform,
+args@{ stdenv, lib, fetchurl, fetchFromGitHub, fetchgit, rustPlatform,
   pkgconfig, cargo, rustc, ncurses,
   # Plugins
   ... }:
 
 let
   self = rustPlatform.buildRustPackage rec {
-    name = "zoomy-${version}";
-    version = "0.0.1";
+    name = "rustodoro-${version}-builder0";
+    version = "rev-4c34dd";
 
     # Haven't upload yet, but usable for nix-shell.
-    src = null;
+    src = fetchgit {
+      url = "/home/fazbdillah/Project/Rustodoro/rustodoro/";
+      rev = "4c34dd67f62be546feb7d2d30991dea080e9e952";
+      sha256 = "0iqi2a3dc57nvr8bc8m70pp7niwb5aqnpji6h80y8im76yxn1637";
+    };
 
-    nativeBuildInputs = [ pkgconfig rustc cargo ];
-    buildInputs = [ ncurses ];
+    nativeBuildInputs = [ pkgconfig rustc cargo ncurses ncurses ];
+    buildInputs = [ ];
 
-    cargoSha256 = "18mfkzdm8wkkf171d3gxsbwnp6m8l30fj4yi6vfi3ys276yv116m";
+    cargoSha256 = "13hh596gl8v2scfqzi3x61zadnb8sizpgrhri85g9fjpjzxfb601";
 
     doCheck = false;
   };
--- distro/nixos/shell.nix
+++ distro/nixos/shell.nix
@@ -1,4 +1,4 @@
-with import <localpkgs> {}; {
+with import <nixos> {}; {
   # Run the command for development,
   # nix-shell --add-root /nix/var/nix/gcroots/per-user/user/timer-rs ./distro/nixos/shell.nix
   self = callPackage ./build.nix {};
